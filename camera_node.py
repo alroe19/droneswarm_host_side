00b0031d100369dcdf0bd2866cab0ca955e39ff1
Alexander Roe <alexanderroe22@hotmail.com>
2025-10-29 17:19:15 +0100

    implemented the host side node

---
+++ camera_node.py
@@ -0,0 +1,48 @@
+
+from rpi_camera_controller import RPiCameraController
+from tcp_client import TCPClient
+
+
+
+def camera_node():
+
+    model_path = "./models/network.rpk"
+    labels_path = "./models/labels.txt"
+    confidence_threshold = 0.6
+    save_images = True
+
+    host = "localhost"
+    port = 5050
+
+    tcp_client = TCPClient(host=host, port=port)
+    tcp_client.connect()
+
+    img_save_interval = 9 # Save an image every 10 frames (0-9)
+
+    camera_controller = RPiCameraController(
+        model_path=model_path,
+        labels_path=labels_path,
+        conf_threshold=confidence_threshold,
+    )
+
+    frame_count = 0
+
+    try:
+        while True:
+            ## Call get_inference and set save_image to True every img_save_interval frames
+            detections = camera_controller.get_inference(save_image=(frame_count % img_save_interval == 0 and save_images))
+            frame_count = (frame_count + 1) % img_save_interval
+            
+            # The get inference can return multiple detections, but we only send the first one for simplicity
+            # This can cause problems if multiple objects are detected, which can get the detection to flicker between them
+            if detections:
+                tcp_client.send(detections[0])
+
+    except KeyboardInterrupt:
+        print("Keyboard interrupt received. Exiting...")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    camera_node()
--- tcp_client.py
+++ tcp_client.py
@@ -1,10 +1,9 @@
 
 import socket
-
-
+from __future__ import annotations
 
 class TCPClient:
-    def __init__(self, host="localhost", port=5050):
+    def __init__(self, host="localhost", port=5050) -> None:
         self._host = host
         self._port = port
         self._client_socket = None
@@ -13,8 +12,8 @@ class TCPClient:
         self._header_size = 64  # Size of the header indicating message length
         self._format = 'utf-8'  # Encoding format
 
-    def connect(self):
-        """ Connect to the TCP server """
+    def connect(self) -> None:
+        """ Connect to the TCP server. Blocks until connection is established. """
         if self._connected:
             return
 
@@ -23,8 +22,8 @@ class TCPClient:
         self._connected = True
 
 
-    def send(self, msg):
-        """ Send a message to the TCP server """
+    def send(self, msg) -> None:
+        """ Send a message to the TCP server. Blocks until the message is sent. """
         if not self._connected:
             raise ConnectionError("Not connected to the server.")
 
@@ -37,7 +36,7 @@ class TCPClient:
         self._client_socket.sendall(len_header)
         self._client_socket.sendall(msg_encoded)
 
-    def __del__(self):
+    def __del__(self) -> None:
         if self._connected:
             self._client_socket.close()
             self._connected = False
