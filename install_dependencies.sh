7637b8db0a4442feeb6f37aa812c345cbc7d1719
Alexander Roe <alexanderroe22@hotmail.com>
2025-12-08 16:43:56 +0100

    Added requirements.txt and bash script for installing dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# THE ABOVE LINE IS REQUIRED FOR THE SCRIPT TO BE EXECUTABLE AS A BASH SCRIPT - DO NOT REMOVE IT!
+# Make this file executable with: chmod +x install_dependencies.sh (just has to be done once)
+# Run this script with: ./install_dependencies.sh
+
